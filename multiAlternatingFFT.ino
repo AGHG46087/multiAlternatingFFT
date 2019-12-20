a2b30f4bda5f1d6dd35b7cd23c0fce62cbafa945
Hans-Jurgen Greiner <grein46087@icloud.com>
2019-12-20 06:28:36 -0700

    added some base comments of the plan

--- multiAlternatingFFT.ino
+++ multiAlternatingFFT.ino
@@ -1,1 +1,10 @@
-// multiAlternatingFFT.ino
+/*
+ * Multi alternating FFT
+ * changes from a 6x10 graph to a 10x6 graph - bottom up and top down
+ * FFT for LED sequrntial Strip, that could be used in horizonal or even vertical plane
+ * by Hans-Jurgen Greiner
+ * 2019
+ * 
+ * base code based on FFT library and code from the Arduino forums
+ * 
+ */
